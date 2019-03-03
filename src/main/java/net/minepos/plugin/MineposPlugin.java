ff50f45d4ba59c9eb79919bc50e633cf31dce6b3
PiggyPiglet <noreply@piggypiglet.me>
2019-03-03 11:55:18 +0800

    Gradle

---
+++ src/main/java/net/minepos/plugin/MineposPlugin.java
@@ -0,0 +1,10 @@
+package net.minepos.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+// ------------------------------
+// Copyright (c) PiggyPiglet 2019
+// https://www.piggypiglet.me
+// ------------------------------
+public final class MineposPlugin extends JavaPlugin {
+}
