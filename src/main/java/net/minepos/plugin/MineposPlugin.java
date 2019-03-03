35e6544b32567f2c8a3e4c44d447f1c88325e715
PiggyPiglet <noreply@piggypiglet.me>
2019-03-03 12:11:51 +0800

    Dependency loading

--- src/main/java/net/minepos/plugin/MineposPlugin.java
+++ src/main/java/net/minepos/plugin/MineposPlugin.java
@@ -1,10 +1,29 @@
 package net.minepos.plugin;
 
+import net.minepos.plugin.core.framework.dependencies.DependencyLoader;
+import net.minepos.plugin.core.framework.dependencies.MavenLibraries;
+import net.minepos.plugin.core.framework.dependencies.MavenLibrary;
 import org.bukkit.plugin.java.JavaPlugin;
 
 // ------------------------------
 // Copyright (c) PiggyPiglet 2019
 // https://www.piggypiglet.me
 // ------------------------------
+@MavenLibraries(
+        value = {
+                @MavenLibrary(groupId = "org.reflections", artifactId = "reflections", version = "0.9.11"),
+                @MavenLibrary(groupId = "javassist", artifactId = "javassist", version = "3.12.1.GA"),
+                @MavenLibrary(groupId = "com.google.inject", artifactId = "guice", version = "4.2.3-SNAPSHOT"),
+                @MavenLibrary(groupId = "javax.inject", artifactId = "javax.inject", version = "1"),
+                @MavenLibrary(groupId = "aopalliance", artifactId = "aopalliance", version = "1.0"),
+                @MavenLibrary(groupId = "org.apache.commons", artifactId = "commons-lang3", version = "3.8.1"),
+                @MavenLibrary(groupId = "commons-io", artifactId = "commons-io", version = "2.6"),
+                @MavenLibrary(groupId = "org.nanohttpd", artifactId = "nanohttpd", version = "2.2.0")
+        }
+)
 public final class MineposPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        DependencyLoader.loadAll(getClass());
+    }
 }
---
+++ src/main/java/net/minepos/plugin/core/framework/dependencies/DependencyLoader.java
@@ -0,0 +1,104 @@
+package net.minepos.plugin.core.framework.dependencies;
+
+import lombok.Data;
+import net.minepos.plugin.MineposPlugin;
+import org.bukkit.Bukkit;
+
+import java.io.File;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.util.logging.Logger;
+
+// ------------------------------
+// Copyright (c) PiggyPiglet 2019
+// https://www.piggypiglet.me
+// ------------------------------
+public final class DependencyLoader {
+    private static final Method ADD_URL_METHOD;
+
+    static {
+        try {
+            ADD_URL_METHOD = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+            ADD_URL_METHOD.setAccessible(true);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void loadAll(Class<?> clazz) {
+        MavenLibrary[] libs = clazz.getDeclaredAnnotationsByType(MavenLibrary.class);
+
+        if (libs != null) {
+            for (MavenLibrary lib : libs) {
+                load(new Dependency(lib.groupId(), lib.artifactId(), lib.version(), lib.repo()));
+            }
+        }
+    }
+
+    @SuppressWarnings({"ConstantConditions", "ResultOfMethodCallIgnored"})
+    private static void load(Dependency d) {
+        Logger logger = Bukkit.getLogger();
+        String name = d.getArtifactId() + "-" + d.getVersion();
+        MineposPlugin main = (MineposPlugin) MineposPlugin.getProvidingPlugin(MineposPlugin.class);
+        File lib = new File(main.getDataFolder() + "/libs/", name + ".jar");
+
+        logger.info(String.format("Loading dependency %s:%s:%s from %s", d.getGroupId(), d.getArtifactId(), d.getVersion(), d.getRepoUrl()));
+
+        if (!lib.exists()) {
+            lib.getParentFile().mkdirs();
+
+            try {
+                logger.info("Dependency " + name + " is not currently downloaded, downloading now.");
+                URL url = d.getUrl();
+
+                try (InputStream is = url.openStream()) {
+                    Files.copy(is, lib.toPath());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (!lib.exists()) {
+            throw new RuntimeException("Unable to download dependency: " + d.toString());
+        }
+
+        URLClassLoader loader = (URLClassLoader) main.getClass().getClassLoader();
+        try {
+            ADD_URL_METHOD.invoke(loader, lib.toURI().toURL());
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to load dependency: " + d.toString());
+        }
+
+        logger.info("Successfully loaded dependency " + name);
+    }
+
+    @Data
+    private static final class Dependency {
+        private final String groupId;
+        private final String artifactId;
+        private final String version;
+        private final String repoUrl;
+
+        public URL getUrl() {
+            String repo = repoUrl;
+
+            if (!repo.endsWith("/")) {
+                repo += "/";
+            }
+
+            repo += "%s/%s/%s/%s-%s.jar";
+
+            try {
+                return new URL(String.format(repo, groupId.replace(".", "/"), artifactId, version, artifactId, version));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+}
