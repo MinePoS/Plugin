5a535531cfc8e27dbc0cad64da25b78d420d0845
PiggyPiglet <noreply@piggypiglet.me>
2019-03-03 11:43:29 +0800

    restart

--- src/main/java/net/minepos/plugin/core/objects/enums/HTTPActions.java
+++
@@ -1,9 +0,0 @@
-package net.minepos.plugin.core.objects.enums;
-
-// ------------------------------
-// Copyright (c) PiggyPiglet 2018
-// https://www.piggypiglet.me
-// ------------------------------
-public enum HTTPActions {
-    RUNCOMMAND
-}
